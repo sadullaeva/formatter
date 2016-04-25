94b48b59a09849b535af3dfc5b93010d00958cf9
Marina Sadullaeva <sadullaeva.marina@gmail.com>
2016-04-26 01:11:27 +0600

    added project structure

---
+++ src/main/java/it/sevenbits/packages/bootstrap/Bootstrap.java
@@ -0,0 +1,20 @@
+package it.sevenbits.packages.bootstrap;
+
+/**
+ * Created by marina on 25.04.16.
+ *
+ */
+public final class Bootstrap {
+
+    private Bootstrap() {
+
+    }
+
+    /**
+     * Main method
+     * @param args - input parameters
+     */
+    public static void main(final String[] args) {
+    }
+
+}
